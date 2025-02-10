e72d4151ec16f45c3b0f5da89d3ddc20788de63d
shantanu1k <shantanukawade2@gmail.com>
2025-02-10 19:48:59 +0530

    Update Theme

--- app/src/main/java/com/cowday/pennywise/ui/theme/Theme.kt
+++ app/src/main/java/com/cowday/pennywise/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.cowday.pennywise.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,25 +11,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = darkGreen,
+    onPrimary = lightGreen,
+    secondary = darkBlue,
+    surface = cyprus
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = green,
+    onPrimary = darkGreen,
+    secondary = blue,
+    surface = honeyDew
 )
 
 @Composable
