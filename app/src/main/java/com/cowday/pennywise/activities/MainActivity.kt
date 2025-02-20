18fe53159c195583954914d1d9d50ceacc1aaa64
shantanu1k <shantanukawade2@gmail.com>
2025-02-20 19:16:44 +0530

    Move screen state mgmt. to activity

--- app/src/main/java/com/cowday/pennywise/activities/MainActivity.kt
+++ app/src/main/java/com/cowday/pennywise/activities/MainActivity.kt
@@ -3,23 +3,108 @@ package com.cowday.pennywise.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import com.cowday.pennywise.ui.common.BottomNavigationItem
 import com.cowday.pennywise.ui.homepage.HomeScreen
+import com.cowday.pennywise.ui.navigation.AnalysisScreen
+import com.cowday.pennywise.ui.navigation.CategoryScreen
 import com.cowday.pennywise.ui.navigation.HomeScreen
+import com.cowday.pennywise.ui.navigation.PennywiseBottomNav
+import com.cowday.pennywise.ui.navigation.PennywiseBottomNavGraph
+import com.cowday.pennywise.ui.navigation.ProfileScreen
+import com.cowday.pennywise.ui.navigation.TransactionScreen
 import com.cowday.pennywise.ui.theme.PennywiseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+            val navController = rememberNavController()
+            val selectedTab = remember { mutableStateOf<PennywiseBottomNav>(HomeScreen) }
+            PennywiseTheme {
+                Scaffold(
+                    bottomBar = {
+                        BottomAppBar(
+                            modifier = Modifier
+                                .clip(
+                                    RoundedCornerShape(
+                                        topStartPercent = 50,
+                                        topEndPercent = 50
+                                    )
+                                ),
+                            contentPadding = PaddingValues(horizontal = 24.dp),
+                            containerColor = MaterialTheme.colorScheme.tertiary,
+                        ) {
+                            BottomNavigationItem(
+                                icon = HomeScreen.icon,
+                                onClick = {
+                                    selectedTab.value = HomeScreen
+                                    navController.navigate(HomeScreen.route)
+                                },
+                                modifier = Modifier.weight(1f),
+                                isSelected = selectedTab.value == HomeScreen
+                            )
+                            BottomNavigationItem(
+                                icon = AnalysisScreen.icon,
+                                onClick = {
+                                    selectedTab.value = AnalysisScreen
+                                    navController.navigate(AnalysisScreen.route)
+                                },
+                                modifier = Modifier
+                                    .weight(1f),
+                                isSelected = selectedTab.value == AnalysisScreen
+                            )
+                            BottomNavigationItem(
+                                icon = TransactionScreen.icon,
+                                onClick = {
+                                    selectedTab.value = TransactionScreen
+                                    navController.navigate(TransactionScreen.route)
+                                },
+                                modifier = Modifier
+                                    .weight(1f),
+                                isSelected = selectedTab.value == TransactionScreen
+                            )
+                            BottomNavigationItem(
+                                icon = CategoryScreen.icon,
+                                onClick = {
+                                    selectedTab.value = CategoryScreen
+                                    navController.navigate(CategoryScreen.route)
+                                },
+                                modifier = Modifier
+                                    .weight(1f),
+                                isSelected = selectedTab.value == CategoryScreen
+                            )
+                            BottomNavigationItem(
+                                icon = ProfileScreen.icon,
+                                onClick = {
+                                    selectedTab.value = ProfileScreen
+                                    navController.navigate(ProfileScreen.route)
+                                },
+                                modifier = Modifier
+                                    .weight(1f),
+                                isSelected = selectedTab.value == ProfileScreen
+                            )
+                        }
+                    },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    PennywiseBottomNavGraph(Modifier.padding(innerPadding), navController)
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/cowday/pennywise/ui/homepage/HomeScreen.kt
+++ app/src/main/java/com/cowday/pennywise/ui/homepage/HomeScreen.kt
@@ -1,138 +1,48 @@
 package com.cowday.pennywise.ui.homepage
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import com.cowday.pennywise.ui.common.BottomNavigationItem
-import com.cowday.pennywise.ui.navigation.AnalysisScreen
-import com.cowday.pennywise.ui.navigation.CategoryScreen
-import com.cowday.pennywise.ui.navigation.HomeScreen
-import com.cowday.pennywise.ui.navigation.PennywiseBottomNav
-import com.cowday.pennywise.ui.navigation.PennywiseBottomNavScreen
-import com.cowday.pennywise.ui.navigation.ProfileScreen
-import com.cowday.pennywise.ui.navigation.TransactionScreen
 import com.cowday.pennywise.ui.theme.PennywiseTheme
 import com.cowday.pennywise.ui.theme.Typography
-import com.cowday.pennywise.ui.theme.green
-import com.cowday.pennywise.ui.theme.poppinsFontFamily
 
 @Composable
-fun HomeScreen() {
-    val navController = rememberNavController()
-    val selectedTab = remember { mutableStateOf<PennywiseBottomNav>(HomeScreen) }
-    PennywiseTheme {
-        Scaffold(
-            topBar = {
-                WelcomeText()
-            },
-            bottomBar = {
-                BottomAppBar(
-                    modifier = Modifier
-                        .clip(
-                            RoundedCornerShape(
-                                topStartPercent = 50,
-                                topEndPercent = 50
-                            )
-                        ),
-                    contentPadding = PaddingValues(horizontal = 24.dp),
-                    containerColor = MaterialTheme.colorScheme.tertiary,
-                ) {
-                    BottomNavigationItem(
-                        icon = HomeScreen.icon,
-                        onClick = {
-                            selectedTab.value = HomeScreen
-                        },
-                        modifier = Modifier.weight(1f),
-                        isSelected = selectedTab.value == HomeScreen
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.primary)
+            .padding(top = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        WelcomeText()
+        Spacer(modifier = Modifier.height(16.dp))
+        FinanceSummary()
+        Spacer(modifier = Modifier.height(32.dp))
+        HomePageTransactionHistory(
+            modifier = Modifier
+                .clip(
+                    RoundedCornerShape(
+                        topStartPercent = 10,
+                        topEndPercent = 10
                     )
-                    BottomNavigationItem(
-                        icon = AnalysisScreen.icon,
-                        onClick = {
-                            selectedTab.value = AnalysisScreen
-                        },
-                        modifier = Modifier
-                            .weight(1f),
-                        isSelected = selectedTab.value == AnalysisScreen
-                    )
-                    BottomNavigationItem(
-                        icon = TransactionScreen.icon,
-                        onClick = {
-                            selectedTab.value = TransactionScreen
-                        },
-                        modifier = Modifier
-                            .weight(1f),
-                        isSelected = selectedTab.value == TransactionScreen
-                    )
-                    BottomNavigationItem(
-                        icon = CategoryScreen.icon,
-                        onClick = {
-                            selectedTab.value = CategoryScreen
-                        },
-                        modifier = Modifier
-                            .weight(1f),
-                        isSelected = selectedTab.value == CategoryScreen
-                    )
-                    BottomNavigationItem(
-                        icon = ProfileScreen.icon,
-                        onClick = {
-                            selectedTab.value = ProfileScreen
-                        },
-                        modifier = Modifier
-                            .weight(1f),
-                        isSelected = selectedTab.value == ProfileScreen
-                    )
-                }
-            },
-            modifier = Modifier.fillMaxSize()
-        ) { innerPadding ->
-            Column(
-                modifier = Modifier
-                    .padding(innerPadding)
-                    .fillMaxSize()
-                    .background(color = MaterialTheme.colorScheme.primary)
-                    .padding(top = 16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                FinanceSummary()
-                Spacer(modifier = Modifier.height(32.dp))
-                HomePageTransactionHistory(
-                    modifier = Modifier
-                        .clip(
-                            RoundedCornerShape(
-                                topStartPercent = 10,
-                                topEndPercent = 10
-                            )
-                        )
                 )
-            }
-        }
+        )
     }
 }
 
