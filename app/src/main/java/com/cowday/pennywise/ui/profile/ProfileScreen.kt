e621cafa5ceb5df532d5d2fbff447e403d2fe14c
shantanu1k <shantanukawade2@gmail.com>
2025-02-21 14:01:30 +0530

    Add Category and Profile screen

--- app/src/main/java/com/cowday/pennywise/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/cowday/pennywise/ui/profile/ProfileScreen.kt
@@ -1,9 +1,23 @@
 package com.cowday.pennywise.ui.profile
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
 fun ProfileScreen(navController: NavController) {
-
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            "Profile screen",
+            style = MaterialTheme.typography.titleMedium
+        )
+    }
 }
