9d067e7924d018d719b0ca22b1da68eb12890cb6
shantanu1k <shantanukawade2@gmail.com>
2025-03-03 14:10:04 +0530

    Update AppBar style

--- app/src/main/java/com/cowday/pennywise/ui/common/AppBar.kt
+++ app/src/main/java/com/cowday/pennywise/ui/common/AppBar.kt
@@ -8,14 +8,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cowday.pennywise.R
+import com.cowday.pennywise.ui.theme.PennywiseTheme
 
 @Composable
 fun AppBar(title: String) {
@@ -28,17 +31,27 @@ fun AppBar(title: String) {
     ) {
         Icon(
             modifier = Modifier
-                .weight(0.2f)
+                .weight(0.1f)
                 .height(16.dp),
             painter = painterResource(R.drawable.icon_back),
             contentDescription = null
         )
         Text(
-            modifier = Modifier.weight(0.6f),
+            modifier = Modifier.weight(0.8f),
             text = title,
             style = MaterialTheme.typography.titleMedium,
             textAlign = TextAlign.Center
         )
-        Spacer(modifier = Modifier.weight(0.2f))
+        Spacer(modifier = Modifier.weight(0.1f))
+    }
+}
+
+@Preview
+@Composable
+private fun AppBarPreview() {
+    PennywiseTheme {
+        Surface {
+            AppBar("Transactions")
+        }
     }
 }
