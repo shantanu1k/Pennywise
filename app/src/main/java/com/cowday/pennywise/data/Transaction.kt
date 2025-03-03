bf008973830285741b5a2b773c08ae9f1e5fbf5b
shantanu1k <shantanukawade2@gmail.com>
2025-03-03 14:09:51 +0530

    Add helper methods for grouping by date

--- app/src/main/java/com/cowday/pennywise/data/Transaction.kt
+++ app/src/main/java/com/cowday/pennywise/data/Transaction.kt
@@ -1,6 +1,13 @@
 package com.cowday.pennywise.data
 
 import java.math.BigDecimal
+import java.text.SimpleDateFormat
+import java.time.YearMonth
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
 open class Transaction(
     open val name: String? = null,
@@ -21,3 +28,45 @@ enum class TransactionType(val strValue: String) {
     INCOME("Income"), EXPENSE("Expense");
 }
 
+private const val DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+
+fun Long.toUtcDate(): String {
+    val date = Date(this)
+    val sdf = SimpleDateFormat(DATE_TIME_FORMAT, Locale.US)
+    sdf.timeZone = TimeZone.getTimeZone("UTC")
+    return sdf.format(date)
+}
+
+data class ExtractedDateAndTime(
+    val year: Int? = null,
+    val month: Int? = null,
+    val day: Int? = null,
+    val hour: Int? = null,
+    val minute: Int? = null,
+    val second: Int? = null,
+)
+
+fun String.extractDateAndTime(): ExtractedDateAndTime {
+    println(this)
+    val formatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)
+    val zonedDateTime = ZonedDateTime.parse(this, formatter)
+
+    return ExtractedDateAndTime(
+        year = zonedDateTime.year,
+        month = zonedDateTime.monthValue,
+        day = zonedDateTime.dayOfMonth,
+        hour = zonedDateTime.hour,
+        minute = zonedDateTime.minute,
+        second = zonedDateTime.second
+    )
+}
+
+fun groupTransactionByDate(transactions: List<Transaction>): Map<String, List<Transaction>> {
+    val formatter = DateTimeFormatter.ofPattern(DATE_TIME_FORMAT)
+
+    return transactions.groupBy {
+        val date = ZonedDateTime.parse(it.date, formatter)
+
+        YearMonth.from(date).format(DateTimeFormatter.ofPattern("MMMM yyyy"))
+    }
+}
