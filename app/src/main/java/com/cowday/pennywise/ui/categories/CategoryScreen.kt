00d52334abdba3bbfcf791dc42154de4f3e01936
shantanu1k <shantanukawade2@gmail.com>
2025-03-08 20:28:04 +0530

    Update Category screen

--- app/src/main/java/com/cowday/pennywise/ui/categories/CategoryScreen.kt
+++ app/src/main/java/com/cowday/pennywise/ui/categories/CategoryScreen.kt
@@ -1,23 +1,53 @@
 package com.cowday.pennywise.ui.categories
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.cowday.pennywise.data.ExpenseCategoryType
+import com.cowday.pennywise.data.IncomeCategoryType
+import com.cowday.pennywise.ui.common.AppBar
+import com.cowday.pennywise.ui.homepage.FinanceSummary
 
 @Composable
 fun CategoryScreen(navController: NavController) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.primary)
+            .padding(top = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            "Category screen",
-            style = MaterialTheme.typography.titleMedium
+        AppBar("Categories")
+        Spacer(modifier = Modifier.height(16.dp))
+        FinanceSummary()
+        Spacer(modifier = Modifier.height(24.dp))
+        CategoryList(
+            categoryList = IncomeCategoryType.entries + ExpenseCategoryType.entries,
+            modifier = Modifier.clip(
+                RoundedCornerShape(
+                    topStartPercent = 10,
+                    topEndPercent = 10
+                )
+            )
         )
     }
+}
+
+@Preview
+@Composable
+private fun CategoryScreenPreview() {
+    CategoryScreen(NavController(LocalContext.current))
 }
