b3e5708067d70bad7ebe15f742ab8d2e85b6a247
shantanu1k <shantanukawade2@gmail.com>
2025-03-12 21:02:19 +0530

    Add custom Date picker

---
+++ app/src/main/java/com/cowday/pennywise/ui/common/DatePickerModal.kt
@@ -0,0 +1,54 @@
+package com.cowday.pennywise.ui.common
+
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerColors
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.cowday.pennywise.ui.theme.PennywiseTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModal(
+    onDateSelected: (Long?) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState()
+
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                onDateSelected(datePickerState.selectedDateMillis)
+                onDismiss()
+            }) {
+                Text("OK")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        },
+    ) {
+        DatePicker(state = datePickerState)
+    }
+}
+
+@Preview
+@Composable
+private fun DatePickerModalPreview() {
+    PennywiseTheme {
+        Surface {
+            DatePickerModal(
+                onDateSelected = {},
+                onDismiss = {}
+            )
+        }
+    }
+}
